93be21d9389a9e2f3963e90723ebb75dc9e7910b
Ashutosh Nandanwar <ashunandanwar98@gmail.com>
2025-04-16 19:32:16 +0530

    Windows app

---
+++ main.js
@@ -0,0 +1,37 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 1000,
+        height: 800,
+        webPreferences: {
+            nodeIntegration: true, // Allows require in frontend JS
+            contextIsolation: false, // Allows access to Node.js APIs
+        },
+    });
+
+    // Load your main HTML file
+    win.loadFile('index.html');
+
+    // Optional: Open DevTools
+    // win.webContents.openDevTools();
+}
+
+// Called when Electron has finished initialization
+app.whenReady().then(() => {
+    createWindow();
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createWindow();
+        }
+    });
+});
+
+// Quit when all windows are closed (except on macOS)
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
